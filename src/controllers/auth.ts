e6a743b615dacda49d13e8de060a57fa8fd02f7e
JohnBen <john@johnben.fr>
2021-04-18 20:23:30 +0200

    correction faute  ortho

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -17,11 +17,11 @@ const signinController = (req: Request, res: Response) => {
 
 const signupController = (req: Request, res: Response) => {
     logging.info(NAMESPACE, 'Signin check route called', req.body)
-    
+
     const { email, password } = req.body
 
     console.log('Log email and paswword in controller => ', email + ' - ' + password);
-    
+
     repo.signup({ email, password }).then(result => {
         logging.debug(NAMESPACE, 'Log value after sql response', result)
         res.status(200).json(result)
@@ -38,10 +38,10 @@ const loggoutController = (req: Request, res: Response) => {
     const userId: number = parseInt(req.params.id)
 
     if(token != '' && userId > 0){
-        repo.Loggout(token, userId).then(result => {
+        repo.loggout(token, userId).then(result => {
             logging.debug(NAMESPACE, 'Log Loggout after sql response', result)
             res.status(200).json(result)
-    
+
         }).catch(err => {
             res.status(404).json(err)
         })
